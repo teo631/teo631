23c93dc85f9b7de66dd5e3075d7b3f06986ce440
teo <teomanakcali01@gmail.com>
2024-04-08 21:57:26 +0300

    ytyt

--- main.js
+++ main.js
@@ -24,6 +24,46 @@ function spagetthicode(cards,cc) {
     // Append the card to the container element
     cc.appendChild(card);
   }
+
+  var link = document.createElement("p");
+  var lc = `
+  Congrulations, you have scrolled so far down that ou have found <a href="/myProjects.html?type=sct">the self-canceling thought wall<a href=""></a>!
+  `;
+  link.innerHTML = lc;
+  link.classList.add("articleParag");
+  cc.appendChild(link);
+}
+
+function IamGoingToRegretTheseInfistrucutreChangesLater(cards,cc) {
+  for (var i = 0; i < cards.sct.length; i++) { // modify this line
+    // Create the card element
+    console.log(i)
+    var card = document.createElement("a");
+    card.classList.add("card_wide");
+
+    // Create the card content
+    var cardContent = `
+      <h1 class="tt">"${cards.sct[i].thought}"</h1>
+    `;
+    sign=1
+    for (var i2 = 0; i2 < cards.sct[i].args.length; i2++) {
+      if(sign>0)
+      {
+        cardContent=cardContent+`<b><i><p class="desc">${cards.sct[i].args[i2]}</p></i></b>`
+      }else{
+        cardContent=cardContent+`<p class="desc">${cards.sct[i].args[i2]}</p>`
+      }
+      
+      sign*=-1
+    }
+
+
+    // Set the card content
+    card.innerHTML = cardContent;
+
+    // Append the card to the container element
+    cc.appendChild(card);
+  }
 }
 
 fetch(buttonClicked+".json")
@@ -54,6 +94,12 @@ fetch(buttonClicked+".json")
       console.log(titleCont)
 
       spagetthicode(cards,cardContainer)
+      return
+    }
+
+    if(buttonClicked=="sct")
+    {
+      IamGoingToRegretTheseInfistrucutreChangesLater(cards,cardContainer)
       return
     }
 
